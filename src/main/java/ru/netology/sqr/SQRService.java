d848ee5c71b945a7e0f877dfa685a98c62b67bf2
Елена Денисова <E-denisova@yandex.ru>
2022-02-14 00:01:52 +0300

    INITIAL COMMIT

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int getSquareRootCount(int rangeLowerBorder,int rangeUpperBorder){
+        int foundCount = 0;
+        for(int sqrCandidate = 10; sqrCandidate < 100; sqrCandidate++){
+            int squared = sqrCandidate * sqrCandidate;
+            // проверяем, годится ли этот кандидат, попадает ли в диапазон
+            if(squared >= rangeLowerBorder && squared <= rangeUpperBorder){
+                foundCount++; //таки попал, увеличим количество найденных
+            }
+        }
+        return foundCount;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.sqr;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvSource(
+            value={
+                    "Range 100 to 200, 100, 200, 5",
+                    "Range 200 to 300, 200, 300, 3",
+                    "Range 100 to 500, 100, 500, 13",
+                    "Range 300 to 400, 300, 400, 3",
+                    "Range 500 to 2000, 500, 2000, 22",
+                    "Range 3000 to 9000, 3000, 9000, 40",
+            },
+            delimiter = ','
+    )
+    void shouldCalculate(String testName, int rangeLowerBorder, int rangeUpperBorder, int expected ){
+       SQRService testService = new SQRService();
+       int foundCnt = testService.getSquareRootCount(rangeLowerBorder, rangeUpperBorder);
+       assertEquals(expected, foundCnt);
+    }
+}
